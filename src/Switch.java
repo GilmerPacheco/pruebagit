59e9712af42a04e6565607209b1c41f0cabaa76b
Gilmer Jesus <gilmerjesus20@gmail.com>
2023-01-04 01:54:31 -0500

    (ADD) ejemplos de sintaxis java

---
+++ src/Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    public static void main(String[] args) {
+        int dia = 4;
+        switch (dia) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Tienes que ir a trabajar");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Quedate en casa");
+                break;
+            default:
+
+        }
+    }
+}
---
+++ src/condicional.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class condicional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        System.out.print("Introduce x: ");
+        x = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Introduce y: ");
+        y = sc.nextInt();
+        int mayor = (x > y) ? x : y; 	// Asigna el valor mayor de X e Y
+        int menor = (x < y) ? x : y; 	// Asigna el valor menor de X e Y
+        //return (x < 25) ? x : -1; 		// Si el valor de X es menor que 25, devuelve
+        // el valor de X, en otro caso, devuelve -1
+        System.out.println("El mayor valor es :"+mayor);
+        System.out.println("El menor valor es :"+menor);
+        if(x % 2 == 0){
+            System.out.println("x es par");
+        }
+        else {
+            System.out.println("x es impar");
+        }
+        if(y % 2 == 0){
+            System.out.println("y es par");
+        }
+        else {
+            System.out.println("y es impar");
+        }
+    }
+}
